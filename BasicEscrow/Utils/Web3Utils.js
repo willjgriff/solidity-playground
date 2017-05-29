df2228fee268af563a755a6e0e1cba74c76bc664
William Griffiths <willjgriff@gmail.com>
2017-05-29 23:59:29 +0100

    Added a couple more tests. Added constants to js tests. Some tests are failing sometimes, need to work out why.

--- BasicEscrow/Utils/Web3Utils.js
+++ BasicEscrow/Utils/Web3Utils.js
@@ -6,9 +6,11 @@ var Web3 = require('web3')
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 var balance = (acct) => { return web3.fromWei(web3.eth.getBalance(acct), 'ether').toNumber() };
+var rawBalance = (acct) => { return web3.eth.getBalance(acct).toNumber() }
 
 module.exports = {
 	balance: balance,
+	rawBalance: rawBalance,
 	bal0: () => balance(web3.eth.accounts[0]),
 	bal1: () => balance(web3.eth.accounts[1]),
 	bal2: () => balance(web3.eth.accounts[2]),
--- BasicEscrow/test/TestEscrow.js
+++ BasicEscrow/test/TestEscrow.js
@@ -3,29 +3,33 @@
 // contract can't easily sign a transaction from an address other than it's own.
 
 var Utils = require("./../Utils/TestUtils.js");
+var Web3Utils = require("./../Utils/Web3Utils.js");
 var Escrow = artifacts.require("./Escrow.sol");
 
 contract("Escrow", () => {
 
 	var escrow;
+	const paymentValue = 1000;
+	const gasPrice = 20000000000;
+	const gasLimit = 500000;
+	var arbiterAddress = web3.eth.accounts[0];
 	var senderAddress = web3.eth.accounts[1];
 	var receiverAddress = web3.eth.accounts[2];
 
 	// To recreate the contract before each test, closer to Unit testing this way.
 	beforeEach(() => {
-		return Escrow.new(senderAddress, receiverAddress)
+		return Escrow.new(senderAddress, receiverAddress, { from: arbiterAddress })
 			.then((instance) => { escrow = instance });
 	});
 
 	it("lockPayment should throw when payment is sent from an address other than the specified sender", () => {
-		Utils.assertThrows(() => { 
-			return escrow.lockPayment({ from: web3.eth.accounts[0], value: 1000, gasPrice: 20000000000, gas: 500000 }) }, 
-				500000);
+		return Utils.assertThrows(() => { 
+			return escrow.lockPayment({ from: arbiterAddress, value: paymentValue, gasPrice: gasPrice, gas: gasLimit }) }, 
+				gasLimit);
 	});
 
 	it("lockPayment should receive payment from the sender address", () => {
-		var paymentValue = 1000;
-		return escrow.lockPayment({ from: senderAddress, value: paymentValue, gasPrice: 20000000000, gas: 500000 })
+		return escrow.lockPayment({ from: senderAddress, value: paymentValue, gasPrice: gasPrice, gas: gasLimit })
 			.then((tx) => {
 				var contractBalance = web3.eth.getBalance(escrow.address);
 				assert.equal(contractBalance.toNumber(), paymentValue, "contract value is not equal to the payment made") 
@@ -33,21 +37,59 @@ contract("Escrow", () => {
 	});
 
 	it("releasePayment should throw when called from an address other than the sender or arbiter", () => {
-		Utils.assertThrows(() => { 
-			return escrow.releasePayment({ from: receiverAddress, gasPrice: 20000000000, gas: 500000 }) }, 
-				500000);
+		return Utils.assertThrows(() => { 
+			return escrow.releasePayment({ from: receiverAddress, gasPrice: gasPrice, gas: gasLimit }) }, gasLimit);
 	});
 
+// This test fails sometimes.
 	it("releasePayment should send the receiver the contract balance when requested by the sender", () => {
-		var paymentValue = 1000;
 		var receiverOriginalBalance = web3.eth.getBalance(receiverAddress).toNumber();
 
-		return escrow.lockPayment({ from: senderAddress, value: paymentValue, gasPrice: 20000000000, gas: 500000 })
-			.then((tx) => { escrow.releasePayment({ from: senderAddress, gasPrice: 20000000000, gas: 500000 }) })
+		return escrow.lockPayment({ from: senderAddress, value: paymentValue, gasPrice: gasPrice, gas: gasLimit })
+			.then((tx) => { return escrow.releasePayment({ from: senderAddress, gasPrice: gasPrice, gas: gasLimit }) })
 			.then((tx) => { 
 				var receiverNewBalance = web3.eth.getBalance(receiverAddress).toNumber();
 				assert.equal(receiverNewBalance, receiverOriginalBalance + paymentValue, "Receivers balance is incorrect") 
-			})
-	})
+			});
+	});
+
+// This test fails sometimes.
+	it("releasePayment should send the receiver the contract balance when requested by the arbiter", () => {
+		var receiverOriginalBalance = web3.eth.getBalance(receiverAddress).toNumber();
+
+		return escrow.lockPayment({ from: senderAddress, value: paymentValue, gasPrice: gasPrice, gas: gasLimit })
+			.then((tx) => { return escrow.releasePayment({ from: arbiterAddress, gasPrice: gasPrice, gas: gasLimit }); })
+			.then((tx) => { 
+				var receiverNewBalance = web3.eth.getBalance(receiverAddress).toNumber();
+				assert.equal(receiverNewBalance, receiverOriginalBalance + paymentValue, "Receivers balance is incorrect") 
+			});
+	});
+
+	it("returnPayment should throw when called from an address other than the arbiter", () => {
+		return Utils.assertThrows(() => {
+			return escrow.returnPayment({ from: senderAddress, gasPrice: gasPrice, gas: gasLimit } )}, gasLimit)
+	});
+
+// This test fails sometimes.
+	it("returnPayment should send contract balance to sender when called by arbiter", () => {
+		var originalSenderBalance = web3.eth.getBalance(senderAddress).toNumber();
+
+		return escrow.lockPayment({ from: senderAddress, value: paymentValue, gasPrice: gasPrice, gas: gasLimit })
+			.then((tx) => { 
+				var senderDeductedBalance = Web3Utils.rawBalance(senderAddress);
+
+				originalSenderBalance -= tx.receipt.gasUsed * gasPrice;
+
+				console.log(originalSenderBalance);
+				console.log(paymentValue);
+				console.log(tx.receipt.gasUsed * gasPrice);
+
+				// assert.equal(senderDeductedBalance, originalSenderBalance - paymentValue - tx.gas, "Senders deducted balance is incorrect");
+				return escrow.returnPayment({ from: arbiterAddress, gasPrice: gasPrice, gas: gasLimit }) })
+			.then((tx) => { 
+				var senderNewBalance = web3.eth.getBalance(senderAddress).toNumber();
+				assert.equal(senderNewBalance, originalSenderBalance, "Senders balance is incorrect");
+			});
+	});
 
 });
