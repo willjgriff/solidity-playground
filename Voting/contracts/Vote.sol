ba78156e4cd9b27dacf64efd50db88ad1ad666a6
William Griffiths <willjgriff@gmail.com>
2017-06-09 18:23:02 +0100

    Converted spaces to tabs.

--- Voting/contracts/Vote.sol
+++ Voting/contracts/Vote.sol
@@ -10,16 +10,14 @@ contract Vote {
 		address delegate;
 	}
 
-    address chairPerson;
-	// Party, represented as uint(party), to number of votes. 
-	// Ideally should not be visible until vote is over, requires some zero-knowledge jazz...
-	// Zero-knowledge jazz also needed to obscure voters from addresses.
+	address chairPerson;
+	// Party, represented as uint(party), to number of votes;
 	mapping(uint => uint) public votes;
 	mapping(address => Voter) voters;
 	
 	modifier onlyChair() {
-	    if (msg.sender != chairPerson) throw;
-	    _;
+		if (msg.sender != chairPerson) throw;
+		_;
 	}
 
 	modifier registeredToVote() {
@@ -28,48 +26,48 @@ contract Vote {
 	}
 	
 	modifier notYetVoted(address who) {
-	    if (voters[who].voted == true) throw;
-	    _;
+		if (voters[who].voted == true) throw;
+		_;
 	}
 	
 	modifier delegatedToVoteFor(address originalVoter) {
-	    if (voters[originalVoter].delegate != msg.sender) throw;
-	    _;
+		if (voters[originalVoter].delegate != msg.sender) throw;
+		_;
 	}
 
 	function Vote() {
-        chairPerson = msg.sender;
+		chairPerson = msg.sender;
 	}
 
 	function registerVoter(address voter) onlyChair {
-        voters[voter].registered = true;
+		voters[voter].registered = true;
 	}
 	
 	function unregisterVoter(address voter) onlyChair {
-	    voters[voter].registered = false;
+		voters[voter].registered = false;
 	}
 
 	function delegateVote(address delegate) registeredToVote {
-	    voters[msg.sender].delegate = delegate;
+		voters[msg.sender].delegate = delegate;
 	}
 
 	function castVote(Party vote) 
-	    registeredToVote 
-	    notYetVoted(msg.sender)
+		registeredToVote 
+		notYetVoted(msg.sender)
 	{
-        castVote(msg.sender, vote);
+		castVote(msg.sender, vote);
 	}
 	
 	function castDelegateVote(address originalVoter, Party vote) 
-	    delegatedToVoteFor(originalVoter)
-	    notYetVoted(originalVoter)
+		delegatedToVoteFor(originalVoter)
+		notYetVoted(originalVoter)
 	{
-	    castVote(originalVoter, vote);
+		castVote(originalVoter, vote);
 	}
 	
 	function castVote(address voter, Party vote) private {
-	    votes[uint(vote)] += 1;
-        voters[voter].voted = true;
+		votes[uint(vote)] += 1;
+		voters[voter].voted = true;
 	}
 
 }
