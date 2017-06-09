fdce2ce50de795c37984037cf95523cb116a8158
William Griffiths <willjgriff@gmail.com>
2017-06-09 13:03:02 +0100

    Added new test and comment regarding test issue.

--- Crowdsale/test/test_crowdsale.js
+++ Crowdsale/test/test_crowdsale.js
@@ -16,6 +16,9 @@ contract("Crowdsale", (accounts) => {
 
 	describe("contribute", () => {
 
+		// This currently fails intermittently. I expect because the event doesn't have the
+		// chance to fire before the contribute.then() is executed. I'm not sure how to 
+		// schedule the .then() until after the event has been fired.
 		it("calls Contributed event with correct arguments", () => {
 			var expectedSender = accounts[1]
 			var expectedContributionAmount = web3.toWei(1, 'ether')
@@ -24,11 +27,32 @@ contract("Crowdsale", (accounts) => {
 
 			var event = crowdsale.Contributed({ sender: expectedSender })
 			event.watch((error, response) => {
+				event.stopWatching()
 				eventSender = response.args.sender
 				eventContributionAmount = response.args.amount
 			})
 
 			return crowdsale.contribute({ from: expectedSender, value: expectedContributionAmount })
+				.then(tx => {
+					assert.equal(eventSender, expectedSender)
+					assert.equal(eventContributionAmount, expectedContributionAmount)
+				})
+		})
+
+		// Probably shouldn't test this in reality. Would be assumed this works.
+		it("doesn't call Contributed event with different index", () => {
+			var expectedSender = ""
+			var expectedContributionAmount = 0
+			var eventSender = ""
+			var eventContributionAmount = 0
+			
+			var event = crowdsale.Contributed({ sender: accounts[1] })
+			event.watch((error, response) => {
+				eventSender = response.args.sender
+				eventContributionAmount = response.args.amount
+			})
+
+			return crowdsale.contribute({ from: accounts[2], value: expectedContributionAmount })
 				.then(tx => {
 					event.stopWatching()
 					assert.equal(eventSender, expectedSender)
