89f3d46d8f848e46ad39b68401c94a71122b3390
William Griffiths <willjgriff@gmail.com>
2017-07-27 22:11:35 +0100

    Remove react project.

--- ReactPlayground/YoutubeTokenReact/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol")
-var YouTubeToken = artifacts.require("./YouTubeToken.sol")
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage)
-  deployer.deploy(YouTubeToken)
-}
