e1f8a5cc665c442306054a351229634e81ccdf87
William Griffiths <willjgriff@gmal.com>
2017-09-14 16:56:12 +0100

    Added test for createCloneToken function. Not yet passing.

--- MiniMeTesting/test/MiniMeTokenTest.js
+++ MiniMeTesting/test/MiniMeTokenTest.js
@@ -1,4 +1,4 @@
-const MiniMeTokenFactory = artifacts.require("MiniMeToken.sol")
+const MiniMeTokenFactory = artifacts.require("./MiniMeTokenFactory.sol")
 const MiniMeToken = artifacts.require("MiniMeToken.sol")
 
 contract("MiniMeToken", accounts => {
@@ -11,14 +11,16 @@ contract("MiniMeToken", accounts => {
                 miniMeTokenFactory = _miniMeTokenFactory
                 return MiniMeToken.new(miniMeTokenFactory.address, 0, 0, "TestToken", 18, "TTN", true)
             })
-            .then(_miniMeToken => miniMeToken = _miniMeToken)
+            .then(_miniMeToken => {
+                miniMeToken = _miniMeToken
+                return miniMeToken.generateTokens(accounts[1], 1000)
+            })
     })
 
     describe("generateTokens(address, uint)", () => {
 
         it("generates expected tokens", () => {
-            return miniMeToken.generateTokens(accounts[1], 1000)
-                .then(() => miniMeToken.balanceOf(accounts[1]))
+            return  miniMeToken.balanceOf(accounts[1])
                 .then(acc1balance => assert.equal(acc1balance, 1000, "Acc1 balance is incorrect, did not generate tokens"))
         })
     })
@@ -26,8 +28,7 @@ contract("MiniMeToken", accounts => {
     describe("destroyTokens(address, uint", () => {
 
         it("destroys expected tokens", () => {
-            return miniMeToken.generateTokens(accounts[1], 1000)
-                .then(() => miniMeToken.destroyTokens(accounts[1], 100))
+            return miniMeToken.destroyTokens(accounts[1], 100)
                 .then(() => miniMeToken.balanceOf(accounts[1]))
                 .then(acc1balance => assert.equal(acc1balance, 900, "Acc1 balance is incorrect, did not destroy tokens"))
         })
@@ -36,10 +37,20 @@ contract("MiniMeToken", accounts => {
     describe("copying MiniMe token via constructor", () => {
 
         it("copies balances", () => {
-            return miniMeToken.generateTokens(accounts[1], 1000)
-                .then(() => MiniMeToken.new(miniMeTokenFactory.address, miniMeToken.address, web3.eth.blockNumber, "TestTokenCopy", 18, "TT2", true))
+            return MiniMeToken.new(miniMeTokenFactory.address, miniMeToken.address, web3.eth.blockNumber, "TestTokenCopy", 18, "TT2", true)
                 .then(miniMeTokenCopy => miniMeTokenCopy.balanceOf(accounts[1]))
-                .then(copiedAcc1Balance => assert.equal(copiedAcc1Balance.toNumber(), 1000, "Acc1 balance is incorrect, token was not copied successfully"))
+                .then(copiedAcc1Balance => assert.equal(copiedAcc1Balance, 1000, "Acc1 balance is incorrect, token was not copied successfully"))
+        })
+    })
+
+    describe("createCloneToken(string cloneTokenName, uint8 cloneDecimalUnits, " +
+        "string cloneTokenSymbol, uint snapshotBlock, bool transfersEnabled)", () => {
+
+        it("copies balances", () => {
+            return miniMeToken.createCloneToken("TestTokenCopy", 18, "TT2", 0, true)
+            // Need to listen for NewCloneToken event to get cloned token address. Not sure how to do that here, will work it out.
+                .then(miniMeTokenCopy => MiniMeToken.at(miniMeTokenCopy).balanceOf(accounts[1]))
+                .then(copiedAcc1Balance => assert.equal(copiedAcc1Balance, 1000, "Acc1 balance is incorrect, token was not copied successfully"))
         })
     })
 
